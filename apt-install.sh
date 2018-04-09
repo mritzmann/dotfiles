a1ce37160c36f140f96c624e8d9ce3f16cd64ff8
Markus Ritzmann <markus@snowflakeops.ch>
2018-04-09 14:19:25 +0200

    remove particular

--- apt-install.sh
+++ apt-install.sh
@@ -2,4 +2,9 @@
 # https://github.com/mritzmann/dotfiles
 
 # install useful packages
-sudo apt-get install curl htop wget traceroute dnsutils pwgen
+sudo apt-get install curl
+sudo apt-get install htop
+sudo apt-get install wget
+sudo apt-get install traceroute
+sudo apt-get install dnsutils
+sudo apt-get install pwgen
