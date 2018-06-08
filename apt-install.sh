f72efdd435d1eaa338cb432af6e3fdf6107a1698
mritzmann <github@markus-mail.com>
2018-06-08 22:52:13 +0200

    add/remove some more software

--- apt-install.sh
+++ apt-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# https://github.com/mritzmann/dotfiles
 
-# install useful packages
+# install useful cli packages
 sudo apt-get install curl
 sudo apt-get install htop
 sudo apt-get install wget
@@ -11,7 +10,5 @@ sudo apt-get install pwgen
 sudo apt-get install tree
 sudo apt-get install ncdu
 sudo apt-get install zsh
-sudo apt-get install filezilla
-sudo apt-get install gparted
-sudo apt-get install vim
 sudo apt-get install nmap
+sudo apt-get install vim
--- apt-remove.sh
+++ apt-remove.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-# https://github.com/mritzmann/dotfiles
 
 # uninstall unwanted packages
 sudo apt-get remove gnome-orca # screen reader
 sudo apt-get remove gnome-maps
 sudo apt-get remove gnome-weather
-sudo apt-get remove aisleriot # game
-sudo apt-get remove gnome-mines # game
-sudo apt-get remove gnome-mahjongg # game
-sudo apt-get remove gnome-sudoku # game
 sudo apt-get remove 'deja-dup*' # backup software
 sudo apt-get remove cheese # photo / webcam software
 sudo apt-get remove gnome-getting-started-docs
 sudo apt-get remove brasero # cd burning software
+sudo apt-get remove gnome-clocks
+
+# some unnecessary games
+sudo apt-get remove aisleriot
+sudo apt-get remove gnome-mines
+sudo apt-get remove gnome-mahjongg
+sudo apt-get remove gnome-sudoku
+sudo apt-get remove gnome-tetravex
+sudo apt-get remove gnome-nibbles
+sudo apt-get remove gnome-robots
+sudo apt-get remove iagno
+sudo apt-get remove lightsoff
+sudo apt-get remove swell-foop
+sudo apt-get remove tali
+sudo apt-get remove quadrapassel
+sudo apt-get remove gnome-taquin
+sudo apt-get remove hitori
+sudo apt-get remove gnome-klotski
