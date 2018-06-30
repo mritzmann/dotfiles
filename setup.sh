ad4d6df8a024ea9f47ec23edcbc864ab6b31f9f8
mritzmann <github@markus-mail.com>
2018-06-30 18:13:39 +0200

    rename files

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 
 # oh my zsh
 rm ~/.zshrc
-ln -s ~/git/dotfiles/zshrc ~/.zshrc
+ln -s ~/git/dotfiles/.zshrc ~/.zshrc
 
 # ms visual studio code
 rm ~/.config/Code/User/settings.json
-ln -s ~/git/dotfiles/mscode.json ~/.config/Code/User/settings.json
+ln -s ~/git/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
