43a287440dd6c31d53a2e9a8999b3a50d4ca50e8
Markus Ritzmann <git@markus-mail.com>
2018-09-14 21:11:42 +0200

    create directory if not exist

--- setup.sh
+++ setup.sh
@@ -11,12 +11,15 @@ ln -s ~/git/dotfiles/.vimrc ~/.vimrc
 
 # ms visual studio code
 rm ~/.config/Code/User/settings.json
+mkdir -p ~/.config/Code/User/
 ln -s ~/git/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
 
 # redshift
 rm ~/.config/redshift.conf
+mkdir -p ~/.config/
 ln -s ~/git/dotfiles/.config/redshift.conf ~/.config/redshift.conf
 
 # newsbeuter
 rm ~/.newsbeuter/config
+mkdir -p ~/.newsbeuter/
 ln -s  ~/git/dotfiles/.newsbeuter/config ~/.newsbeuter/config
