735b89eeedcd6d2fee71a8ada752436fabdc7df1
Markus Ritzmann <markus@opsone.ch>
2018-11-30 12:05:39 +0100

    replace i3status with i3blocks

--- modules/i3/manifests/init.pp
+++ modules/i3/manifests/init.pp
@@ -1,5 +1,7 @@
 # install i3 and config
 class i3 {
+
+  # i3 main package
   package {
     'i3': ensure => installed;
   }
@@ -12,7 +14,25 @@ class i3 {
 
   file { "/home/${::myuser}/.config/i3/config":
     ensure  => 'file',
-    content => template('i3/config'),
+    content => template('i3/i3.conf'),
+    owner   => "$::myuser",
+    group   => "$::myuser",
+  }
+
+  # i3blocks
+  package {
+    'i3blocks': ensure => installed;
+  }
+
+  file { "/home/${::myuser}/.config/i3blocks":
+    ensure  => 'directory',
+    owner   => "$::myuser",
+    group   => "$::myuser",
+  }
+
+  file { "/home/${::myuser}/.config/i3blocks/config":
+    ensure  => 'file',
+    content => template('i3/i3blocks.conf'),
     owner   => "$::myuser",
     group   => "$::myuser",
   }
